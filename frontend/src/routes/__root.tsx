4998b46f63d63710fb2d9180ee79b4e0426a685c
tusharbanga <168979024+tusharbanga@users.noreply.github.com>
2026-07-26 12:14:39 +0530

    25

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -156,7 +156,7 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
+      {}
       <Outlet />
     </QueryClientProvider>
   );
